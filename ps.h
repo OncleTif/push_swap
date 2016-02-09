6c8e6d0fcf130284b0036243f3dfa078db06990d
tmanet <tmanet@student.42.fr>
2016-02-09 12:46:25 +0100

    ajout de move

--- ps.h
+++ ps.h
@@ -6,13 +6,15 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/08 15:55:39 by tmanet            #+#    #+#             */
-/*   Updated: 2016/02/09 11:32:20 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/02/09 12:44:28 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PS_H
 # define PS_H
 # include "libft/libft.h"
+
+typedef struct s_move	t_move;
 
 typedef struct	s_stck
 {
@@ -21,6 +23,12 @@ typedef struct	s_stck
 	t_list	*a;
 	t_list	*b;
 }				t_stck;
+
+struct			s_move
+{
+	t_stck		*stck;
+	t_move		*next;
+};
 
 t_list	*ft_atolst(char*str);
 void	ft_print_stack_a(t_stck *stck);
