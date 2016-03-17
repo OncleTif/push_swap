3cb3b103c59e421063fe2fa371224821cb88ff2e
tmanet <tmanet@student.42.fr>
2016-03-17 16:50:32 +0100

    correction de print

--- ft_print_stack_a.c
+++ ft_print_stack_a.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/09 11:30:56 by tmanet            #+#    #+#             */
-/*   Updated: 2016/03/17 14:38:46 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/03/17 16:49:09 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,20 @@
 void	ft_print_stack_a(t_stck *stck)
 {
 	int	pos;
+	t_list	*elem;
 
 	pos = 0;
-	while (stck->a)
+	elem = stck->a;
+	while (elem)
 	{
 		ft_putstr("value : ");
-		ft_putnbr(((t_ntry*)stck->a->content)->value);
+		ft_putnbr(((t_ntry*)elem->content)->value);
 		ft_putstr(" final position : ");
-		ft_putnbrendl(((t_ntry*)stck->a->content)->goal);
+		ft_putnbrendl(((t_ntry*)elem->content)->goal);
 		//ft_putnbr(stck->ntry[pos].goal);
 		//ft_putstr(" offset : ");
 		//ft_putnbrendl(stck->ntry[pos].offset);
-		stck->a = stck->a->next;
+		elem = elem->next;
 		pos++;
 	}
 }
--- ft_resolve.c
+++ ft_resolve.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/14 15:44:47 by tmanet            #+#    #+#             */
-/*   Updated: 2016/03/17 15:53:20 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/03/17 16:49:21 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@ void	ft_resolve(t_stck *stck)
 	int	i;
 
 	i = 0;
-	while (i < stck->size)
+	while (i < stck->size - 1)
 	{
 		ft_push_range_to_b(stck, i);
 		i++;
+		ft_putchar('\n');
+		ft_print_stack_a(stck);
 	}
 	ft_push_b_to_a(stck);
 }
