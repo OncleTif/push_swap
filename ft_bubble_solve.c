66886894dfbf8b5171d03f3f71f50cb9327f4a65
tmanet <tmanet@student.42.fr>
2016-03-21 10:54:57 +0100

    modif du message de doublon

--- ft_bubble_solve.c
+++ ft_bubble_solve.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/10 10:42:36 by tmanet            #+#    #+#             */
-/*   Updated: 2016/03/21 10:53:18 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/03/21 10:54:28 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	ft_bubble_solve(t_stck *stck)
 				((t_ntry*)elem->content)->goal++;
 			if (((t_ntry*)elem->content)->value ==
 					((t_ntry*)ref->content)->value && elem != ref)
-				ft_error("Error, the list contains a number twice");
+				ft_error("Error: the list contains a number twice");
 			ref = ref->next;
 		}
 		elem = elem->next;
