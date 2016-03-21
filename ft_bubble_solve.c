6bf8f2b514bfa40a8bd303a4ae84b290248c9b34
tmanet <tmanet@student.42.fr>
2016-03-21 10:53:38 +0100

    ajout detection doublon

--- ft_bubble_solve.c
+++ ft_bubble_solve.c
@@ -6,7 +6,7 @@
 /*   By: tmanet <tmanet@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/02/10 10:42:36 by tmanet            #+#    #+#             */
-/*   Updated: 2016/03/17 14:37:08 by tmanet           ###   ########.fr       */
+/*   Updated: 2016/03/21 10:53:18 by tmanet           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,9 @@ void	ft_bubble_solve(t_stck *stck)
 			if (((t_ntry*)elem->content)->value <
 					((t_ntry*)ref->content)->value)
 				((t_ntry*)elem->content)->goal++;
+			if (((t_ntry*)elem->content)->value ==
+					((t_ntry*)ref->content)->value && elem != ref)
+				ft_error("Error, the list contains a number twice");
 			ref = ref->next;
 		}
 		elem = elem->next;
