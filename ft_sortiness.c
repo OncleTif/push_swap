decdfc8a085dfd4926d1fa812678ce0d59a938dc
tmanet <tmanet@student.42.fr>
2016-03-24 12:47:38 +0100

    sortiness ok

--- ft_sortiness.c
+++ ft_sortiness.c
@@ -2,10 +2,18 @@
 
 int	ft_sortiness(t_stck *stck)
 {
-t_list	*elem;
+	t_list	*elem;
 
-elem = stck->a;
-
-
-return (stck->end - stck->bgn);
+	elem = stck->a;
+	while (elem)
+	{
+		if (((t_ntry*)elem->content)->goal != ((t_ntry*)elem->content)->init)
+		{
+			stck->end = ((t_ntry*)elem->content)->init;
+			if (!stck->bgin)
+				stck->bgin = ((t_ntry*)elem->content)->init;
+		}
+		elem = elem->next;
+	}
+	return (stck->end - stck->bgin);
 }
